43b66d4b21d54514b237146ebd685cc5c2681433
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2023-11-19 23:16:33 -0500

    Initial Commit

---
+++ Pokemon Card Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokemon Card Tracker
+//
+//  Created by Joaquin Castrillon on 11/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pokemon Card Tracker/Pokemon_Card_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_Card_TrackerApp.swift
+//  Pokemon Card Tracker
+//
+//  Created by Joaquin Castrillon on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_Card_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
